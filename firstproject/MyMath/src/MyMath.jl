f35f40e56b2d903b9a5d83f7c1b31824e03e4962
Bill Crupi <crupib@gmail.com>
2025-07-19 08:25:40 -0400

    first project

---
+++ firstproject/MyMath/src/MyMath.jl
@@ -0,0 +1,9 @@
+module MyMath  
+export add, multiply
+function add(a::Number, b::Number)
+      return a + b  
+end  
+function multiply(a::Number, b::Number)
+      return a * b
+end
+end
---
+++ firstproject/MyMath/test/runtests.jl
@@ -0,0 +1,5 @@
+using Test
+using MyMath
+
+@test add(1, 2) == 3
+
